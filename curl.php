8f1dc0a726c60a10ca806bb774242d82f6bb05d9
deniirawan2502 <60250415+deniirawan2502@users.noreply.github.com>
2020-01-25 15:03:05 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -10,8 +10,8 @@ $header[] = "X-AppVersion: 3.45.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: en_ID";
-$header[] = "X-Location: -6.246265,106.690718";
-$header[] = "X-Location-Accuracy: 3.0";
+$header[] = "X-Location: 3.6675651,98.6545161";
+$header[] = "X-Location-Accuracy: 5.0";
 if ($pin):
 $header[] = "pin: $pin";
     endif;
